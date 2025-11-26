a38af30eee09f5eae51bc8a4f3ce11ef060ac675
Maryam123-oss <amad321a@icloud.com>
2025-11-26 19:53:52 +0300

    vv1

---
+++ src/Arrays/Main.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Main {
+    public static void main(String[] args){
+Arrayss arr=new Arrayss();
+int []numbers=new int[5];
+arr.input(numbers);
+        arr.Traversal(numbers);
+
+
+
+}}
