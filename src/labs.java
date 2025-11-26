17fd54cb3ff629fb2fcda1b8b40b2928ce3e7181
Maryam123-oss <amad321a@icloud.com>
2025-11-26 19:22:50 +0300

    v1

--- src/labs.java
+++ src/labs.java
@@ -2,6 +2,6 @@ import java.security.spec.RSAOtherPrimeInfo;
 
 public class labs {
     public static void main(String[] args) {
-       System.out.println("labs");
+       System.out.println("lab2");
     }
 }
