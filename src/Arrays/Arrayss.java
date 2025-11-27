0e936fd2c3e15e8d37fadc1037726a783365e9ce
Maryam123-oss <amad321a@icloud.com>
2025-11-27 06:10:38 +0300

    v2

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -3,20 +3,42 @@ package Arrays;
 import java.util.Scanner;
 
 public class Arrayss {
-    Scanner scanner=new Scanner(System.in) ;
+    Scanner scanner = new Scanner(System.in);
 
-    public void input(int []a){
-        System.out.println("please enter "+a.length+"elements");
-        for (int i=0;i<a.length;i++)
-            a[i]=scanner.nextInt();
-
-
+   /* public void input(int[] a) {
+        System.out.println("Please enter " + a.length + " elements:");
+        for (int i = 0; i < a.length; i++)
+            a[i] = scanner.nextInt();
     }
-    public void Traversal(int []a){
-        System.out.println("******The element are:");
-        for (int i=0;i<a.length;i++)
-            System.out.println("index["+i+"]=" +a[i]);
+*/
+    public void Traversal(int[] a) {
+        System.out.println("****** The elements are:");
+        for (int i = 0; i < a.length; i++)
+            System.out.println("index[" + i + "] = " + a[i]);
+    }
 
+    public void update(int[] a, int index, int nvalue) {
+        if (index < 0 || index >= a.length)
+            System.out.println("Index not found");
+        else
+            a[index] = nvalue;
+    }
 
+    public void update(int[] a, int index) {
+        if (index < 0 || index >= a.length)
+            System.out.println("Index not found");
+        else
+            a[index] = 0;
+    }
+    public void shiftLeft_delete(int[] a, int index) {
+        if (index < 0 || index >= a.length) {
+            System.out.println("Index not found");
+            return;
+        }
+
+        for (int i = index; i < a.length - 1; i++)
+            a[i] = a[i + 1];
+
+        a[a.length - 1] = Integer.MIN_VALUE;
     }
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -3,9 +3,14 @@ package Arrays;
 public class Main {
     public static void main(String[] args){
 Arrayss arr=new Arrayss();
-int []numbers=new int[5];
-arr.input(numbers);
+int []numbers={1,2,3,4,5};
+//arr.input(numbers);
+//arr.Traversal(numbers);
+
+
+        arr.shiftLeft_delete(numbers,2);
         arr.Traversal(numbers);
+        //arr.update(numbers,9,4);
 
 
 
